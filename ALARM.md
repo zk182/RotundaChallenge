eac5238f47c806161af23962e00129045c52e0fe
Nicolas Battaglia <nicobattmdq@gmail.com>
2023-03-02 14:49:58 -0300

    alarm completed

---
+++ ALARM.md
@@ -0,0 +1,41 @@
+alarm.js is a prototype in pseudo code of the solution for the alarm problem
+
+We defined an entry endpoint (POST /operation) which calls coolOperation.
+coolOperation is the function responsible for doing normal operations that we need to catch, 
+but in our code, this just returns an error simulating something going wrong in our
+business logic.
+
+Our endpoint then:
+a) logs the error by calling logError()
+b) checks status of shouldNotify, which ensures that at least 60 seconds passed since last email notification
+c) calls hasErrorOverflow() in case shouldNotify is enabled
+d) calls notify() which finally sends the email notification, in case errorOverflow is true
+
+hasErrorOverflow then reads last 10 error lines 
+(another solution could be to standarize every error information to N bytes each, and then reading last N x 10 bytes of the file)
+which returns true/false based on how much errors happened during last 60 seconds
+
+In case less than 60 seconds passed in the ten error period, we call notify() which:
+a) sets shouldNotify to false
+b) puts a timeout on shouldNotify to reset it after 60 seconds
+c) sends the email (in pseudocode, line 60)
+
+
+In case other operations needs to be aware of this as well, 
+email handling should be defined in an error middleware.
+Every operation that needs to send an email should call
+next(err), where the error middleware should be defined as
+
+function emailHandler(err, req, res, next) {
+    await logError(err);
+    if (shouldNotify) { 
+      await checkErrors();
+    }
+    res.send('Error logged');  
+}
+
+and should be defined as the last middleware in the chain
+
+app.use(someMiddleware);
+app.use(someOtherMiddleware);
+app.use(emailHandler);
--- alarm.js
+++ alarm.js
@@ -13,37 +13,56 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 })
 
-app.post('/error', async (req, res) => {
-  await logError({ someproperty: 'error property'});
-  console.log('should notify is', shouldNotify);
-  if (shouldNotify) { 
-    await checkErrors();
+app.post('/operation', async (req, res) => {
+  try { 
+    coolOperation()
+  } catch (err) {
+    await logError(err);
+    if (shouldNotify) { 
+      const errorOverflow = await hasErrorOverflow();
+      if (errorOverflow) await notify();
+    }
+  } finally {
+    res.send('Error logged');  
   }
-  res.send('Error logged');  
 })
 
+// This is the function responsible for doing normal operations that we need to catch
+function coolOperation() {
+  throw new Error({ someproperty: 'this is an error property'})
+}
 
+// This is the logger function responsible for appending errors
 async function logError(error) {
     fs.appendFile("/tmp/test", `${error.someproperty},${new Date()}${os.EOL}`, function(err) {
         if (!err) console.log("The error was saved!");
     }); 
 }
 
-async function checkErrors() {
+async function hasErrorOverflow() {
     const errors = await readLastLines.read('/tmp/test', 10).then((lines) => lines.split('\n')); // we just read only last ten items, not the whole file
     if (errors.length == maxPossibleErrors + 1) { // we have ten items + one empty line
         const first = new Date(errors[0]);
         const last = new Date(errors[9]);
         const seconds = Math.abs(first.getTime() - last.getTime()) / 1000;
-        console.log(`difference in seconds is ${seconds}, ${first.getTime()}, ${last.getTime()}`);
-        if (seconds <= 60) { // the ten error items must have passed in the last minute
-          resetNotifier();
-          console.log('email sent!');
-        }
+        console.log(`difference in seconds is ${seconds}`);
+        return seconds <= 60; // the ten error items must have passed in the last minute
     }
+    return false;
+}
+
+
+async function notify() {
+  resetNotifier();        
+  // here we should send an email, but since this is pseudo code we 
+  // can think of next line as an email sender
+
+  // sendEmail(user);
+  console.log('email sent!');
 }
 
 async function resetNotifier() {
   shouldNotify = false;
-  setTimeout(()=> { shouldNotify = true; console.log('hi time has passed!!')}, 60000);
+  console.log('shouldNotify is now disabled');
+  setTimeout(()=> { shouldNotify = true; console.log('shouldNotify is now enabled')}, 60000);
 }
